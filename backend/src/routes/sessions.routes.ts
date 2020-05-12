3383bb9aae089e3ace903948bc9f9563c4f99b97
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-05-12 03:03:33 -0300

    config auth with jwt

---
+++ backend/src/routes/sessions.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+    const authenticateUser = new AuthenticateUserService();
+    const { user, token } = await authenticateUser.execute({ email, password })
+    const { name } = user
+    return response.json({ name, token });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
