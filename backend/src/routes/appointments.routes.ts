8f0904da366aff7da197e6e583cbb3c58b0efe4e
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-05-12 01:37:16 -0300

    hash password

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -15,10 +15,10 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
     const dateIso = parseISO(date);
     const createAppointment = new CreateAppointmentService();
-    const appointment = await createAppointment.execute({ date: dateIso, provider });
+    const appointment = await createAppointment.execute({ date: dateIso, provider_id });
 
     return response.json(appointment);
   } catch (err) {
