4cd1f134ec01ed507d7e8fb642eabf70ac8fe3fb
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-05-12 20:53:32 -0300

    handle errors

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -17,16 +17,12 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
-    const dateIso = parseISO(date);
-    const createAppointment = new CreateAppointmentService();
-    const appointment = await createAppointment.execute({ date: dateIso, provider_id });
+  const { provider_id, date } = request.body;
+  const dateIso = parseISO(date);
+  const createAppointment = new CreateAppointmentService();
+  const appointment = await createAppointment.execute({ date: dateIso, provider_id });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -5,15 +5,11 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
-    const authenticateUser = new AuthenticateUserService();
-    const { user, token } = await authenticateUser.execute({ email, password })
-    const { name } = user
-    return response.json({ name, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  const { email, password } = request.body;
+  const authenticateUser = new AuthenticateUserService();
+  const { user, token } = await authenticateUser.execute({ email, password })
+  const { name } = user
+  return response.json({ name, token });
 });
 
 export default sessionsRouter;
