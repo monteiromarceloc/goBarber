cc0c004dba05977ac8df4680090ee0ace6862049
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-06-13 15:27:53 -0300

    fix eslinf config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <div className="App">
-    Hello World
-  </div>
+  <>
+    <div className="App">
+      Hello World
+    </div>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
